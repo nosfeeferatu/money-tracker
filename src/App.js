39606f70bd2ab30ed9b9b4ab772e2f43da370800
Naseef Mashood <nosfeeferatu@gmail.com>
2023-09-28 01:35:50 +0530

    Basic HTML Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section id='hero'>
+        <h1>What's in our Wallets Now?</h1>
+        <p className='tagline'>Track your spending and income</p>
+      </section>
+
+      <section id='inputTransaction'>
+        <h2>₹500</h2>
+        <form>
+          <div className='basics'>
+            <input type='text' placeholder='Dinner?' />
+            <input type='datetime-local'/>
+          </div>
+          <div className='description'>
+            <textarea placeholder='Description'></textarea>
+          </div>
+        </form>
+      </section>
+
+      <section id='allTransactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <h3>Breakfast</h3>
+            <p>That Masala Dosa was so good.</p>
+          </div>
+          <div className='right'>
+            <h3>₹60</h3>
+            <p>28-09-2023 8:14</p>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className='left'>
+            <h3>Coffee</h3>
+            <p>A break from work.</p>
+          </div>
+          <div className='right'>
+            <h3>₹10</h3>
+            <p>28-09-2023 10:57</p>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className='left'>
+            <h3>Money Returned</h3>
+            <p>What Alex owed me.</p>
+          </div>
+          <div className='right'>
+            <h3>₹500</h3>
+            <p>28-09-2023 16:23</p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
